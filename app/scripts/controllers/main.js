0e9db3627bd3cb37021f05ad467e26903455dff3
Feyzo <feyzullahdaldal@gmail.com>
2014-10-12 18:12:47 +0300

    Ran "yo angular"
    https://github.com/yeoman/generator-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deusExStateMachinePortalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deusExStateMachinePortalApp
+ */
+angular.module('deusExStateMachinePortalApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
