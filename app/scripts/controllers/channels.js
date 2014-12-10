a9b03421042d597a9d0e3375fdf540e62265e1b7
Feyzo <feyzullahdaldal@gmail.com>
2014-12-11 00:33:23 +0200

    Spark activation

--- app/scripts/controllers/channels.js
+++ app/scripts/controllers/channels.js
@@ -10,23 +10,27 @@
 angular.module('deusExStateMachinePortalApp')
     .controller('ChannelsCtrl', function($location, $scope, Session, $state, dataService, username) {
     	$scope.channels = {
-    		twitter: 'notactive',
-    		spark: 'notactive'
+    		twitter: {
+    			status: 'notactive'
+    		},
+    		spark: {
+    			status: 'notactive'
+    		}
     	};
 
-        $scope.activate = function (channelname) {
+        $scope.activate = function (channelname, accessToken) {
         	if(channelname === 'twitter') {
-        		$scope.channels.twitter = 'loading';
+        		$scope.channels.twitter.status = 'loading';
 
         		//Start listening for child windows
         		window.channelListener = {
         			done: function (error, data) {
         				if(error) {
-        					$scope.channels.twitter = 'notactive';
+        					$scope.channels.twitter.status = 'notactive';
         					alertify.error(error);
         				} else {
 							dataService.saveChannelData(username, channelname, data).then(function() {
-								$scope.channels.twitter = 'success';
+								$scope.channels.twitter.status = 'success';
 								alertify.success('"' + channelname  + '" channel activated.');
 							});
         				}
@@ -35,6 +39,12 @@ angular.module('deusExStateMachinePortalApp')
 
         		window.open('/channels/twitter', '_blank');
         	} else if(channelname === 'spark') {
+        		if(accessToken) {
+        			console.log(accessToken);
+        		} else {
+        			window.open('https://www.spark.io/build/new', '_blank');
+        			$scope.channels.spark.status = 'loading';
+        		}
         		
         	}
         };
