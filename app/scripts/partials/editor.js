7b85e4bce881c9583dac8f7a522e838476d08c71
Feyzo <feyzullahdaldal@gmail.com>
2015-01-12 17:15:37 +0200

    Creating new statechart fixed

--- app/scripts/partials/editor.js
+++ app/scripts/partials/editor.js
@@ -8,64 +8,70 @@
  * Controller of the deusExStateMachinePortalApp
  */
 angular.module('deusExStateMachinePortalApp')
-    .controller('EditorCtrl', function($scope, $state, simulateService, dataService, chartName, chartContent, username) {
-        $scope.chartName = chartName;
-        $scope.chartContent = chartContent.data;
-        $scope.username = username;
+  .controller('EditorCtrl', function ($scope, $state, simulateService, dataService, chartName, chartContent, username) {
+    $scope.chartName = chartName;
+    $scope.chartContent = chartContent.data;
+    $scope.username = username;
 
-        simulateService.update($scope.chartContent);
+    simulateService.update($scope.chartContent);
 
-        $scope.aceChanged = function() {
-            simulateService.update($scope.chartContent);
-        };
+    $scope.aceChanged = function () {
+      simulateService.update($scope.chartContent);
+    };
 
-        $scope.saveStatechart = function(content) {
-            checkChartContent(content, function (error, name) {
-                if(error) {
-                    alertify.error(error);
-                    return;
-                }
-                
-                dataService.createStateChart(username, content).then(function() {
-                    simulateService.chartSaved(name);
-
-                    $state.go('main.charts.detail', { chartName: name }, { reload: true });
-                    alertify.success('Statechart saved');
-                }, function(response) {
-                    if (response.data.message) {
-                        alertify.error(response.data.message);
-                    } else {
-                        alertify.error('An error occured');
-                    }
-                });
-            });
-        };
-
-        function checkChartContent(content, done) {
-
-            if (!content || content.length === 0) {
-                done('Please enter code for your Statechart');
-                return;
-            }
-
-            var doc;
-
-            try {
-                doc = (new DOMParser()).parseFromString(content, 'application/xml');
-
-                if(doc.getElementsByTagName('parsererror').length){
-                  throw({
-                    //Only div in parsererror contains the error message
-                    //If there is more than one error, browser shows only the first error
-                    message: $(doc).find('parsererror div').html()
-                  });
-                }
-            } catch (e) {
-                done(e.message);
-                return;
-            }
-
-            done(null, doc.documentElement.getAttribute('name'));
-            return;
+    $scope.saveStatechart = function (content) {
+      checkChartContent(content, function (error, name) {
+        if (error) {
+          alertify.error(error);
+          return;
         }
-    });
+
+        dataService.createStateChart(username, content).then(function (result) {
+          simulateService.chartSaved(name);
+
+          $state.go('main.charts.detail', {
+            chartName: name,
+            chartId: result.data.chartId
+          }, {
+            reload: true
+          });
+          alertify.success('Statechart saved');
+        }, function (response) {
+          if (response.data.message) {
+            alertify.error(response.data.message);
+          } else {
+            alertify.error('An error occured');
+          }
+        });
+      });
+    };
+
+    function checkChartContent(content, done) {
+
+      if (!content || content.length === 0) {
+        done('Please enter code for your Statechart');
+        return;
+      }
+
+      var doc;
+
+      try {
+        doc = (new DOMParser()).parseFromString(content, 'application/xml');
+
+        if (doc.getElementsByTagName('parsererror').length) {
+          throw ({
+            //Only div in parsererror contains the error message
+            //If there is more than one error, browser shows only the first error
+            message: $(doc).find('parsererror div').html()
+          });
+        }
+      } catch (e) {
+        done(e.message);
+        return;
+      }
+
+      done(null, doc.documentElement.getAttribute('name'));
+      return;
+    }
+  });
+
